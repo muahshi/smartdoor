c5d04fc3e9bd648d6c5c9a2b020b23be72ac4a20
Muahshi Digital <muahshi.mubi@gmail.com>
2026-07-31 17:37:33 +0530

    Add files via upload

--- android/app/src/main/java/in/mysmartdoor/app/ui/components/SDBottomNavigation.kt
+++ android/app/src/main/java/in/mysmartdoor/app/ui/components/SDBottomNavigation.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.vector.ImageVector
