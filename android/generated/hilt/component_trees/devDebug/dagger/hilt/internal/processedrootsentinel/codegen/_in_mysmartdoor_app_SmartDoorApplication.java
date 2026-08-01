879b509e22f83ba8fa0b785cfaad488fc3a151ec
Muahshi Digital <muahshi.mubi@gmail.com>
2026-08-01 11:17:50 +0530

    Delete android/generated directory

--- android/generated/hilt/component_trees/devDebug/dagger/hilt/internal/processedrootsentinel/codegen/_in_mysmartdoor_app_SmartDoorApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "in.mysmartdoor.app.SmartDoorApplication"
-)
-public final class _in_mysmartdoor_app_SmartDoorApplication {
-}
