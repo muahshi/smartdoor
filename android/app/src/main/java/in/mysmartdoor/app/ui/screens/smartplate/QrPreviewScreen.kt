00c34358a653ff8487dc2ee01d477da781575b37
Muahshi Digital <muahshi.mubi@gmail.com>
2026-08-02 20:08:17 +0530

    Update QrPreviewScreen.kt

--- android/app/src/main/java/in/mysmartdoor/app/ui/screens/smartplate/QrPreviewScreen.kt
+++ android/app/src/main/java/in/mysmartdoor/app/ui/screens/smartplate/QrPreviewScreen.kt
@@ -53,6 +53,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
